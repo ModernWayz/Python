c49fadff441e4b6b69ff605297339e161aaedb28
Axel De Roeck <axelderoeck22@gmail.com>
2022-06-22 17:22:41 +0200

    move

--- Labo01/oplossingen_labo1/oef3.py
+++
@@ -1,9 +0,0 @@
-## Oefening 3: # Hallo!
-# Schrijf een programma dat de gebruiker naar zijn of haar naam vraagt. 
-# Daarna vraagt het programma naar de interesses van de gebruiker. Het 
-# programma antwoordt vervolgens met een hallo-boodschap waarin de 
-# gevraagde gegevens verwerkt zitten.
-
-naam = input("Geef uw naam: ")
-interesses = input("Geef uw interesses: ")
-print(f"Hallo {naam}! We zijn blij met iemand die interesse heeft voor {interesses}!")
--- Labo01/oplossingen_labo1/oef6.py
+++
@@ -1,9 +0,0 @@
-## Oefening 6: # Som van de eerste *n* positieve gehele getallen
-# Schrijf een programma dat een positief geheel getal als input vraagt 
-# en dan als resultaat de som weergeeft van alle gehele getallen van 1 
-# tot *n*. Je kan dit berekenen met de volgende formule:
-# som = (n)*(n+1)/2
-
-getal = int(input("Geef een positief geheel getal: "))
-som_van_de_getallen = (getal * (getal+1))/2 
-print(f"De som van alle positieve gehele getallen tot {getal} is {int(som_van_de_getallen)}")
